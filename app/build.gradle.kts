59f28ecf0e5a1dc37f5ac6b062b48605229d2651
heisenricher <heisenricher@gmail.com>
2026-05-25 14:51:59 +0530

    Fix AAPT image compilation error by disabling PNG crunching

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isCrunchPngs = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("debug")
         }
