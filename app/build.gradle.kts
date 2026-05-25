66e2eadb6ca9995b084b91b9c787ca8a989f6589
heisenricher <heisenricher@gmail.com>
2026-05-25 14:10:02 +0530

    Sign release APK with debug key for immediate installation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ android {
         release {
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     
